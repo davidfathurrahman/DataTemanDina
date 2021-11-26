ef48a3509981e1007a3845c91d8137a71a7f296b
davidfathurrahman <92985746+davidfathurrahman@users.noreply.github.com>
2021-11-26 15:14:00 +0700

    DataTemanDina
    Praktikum 9

---
+++ app/src/main/java/com/dina/datateman/MyFriendAdapter.kt
@@ -0,0 +1,34 @@
+package com.dina.datateman
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.ScrollingTabContainerView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.my_friends_item.*
+
+class MyFriendAdapter(private val context: Context, private val items: ArrayList<MyFriend>):
+    RecyclerView.Adapter<MyFriendAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=ViewHolder(
+        LayoutInflater.from(context).inflate(R.layout.my_friends_item,parent,false)
+    )
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+       holder.bindItem(items.get(position))
+    }
+
+    class ViewHolder(override val containerView:View):
+            RecyclerView.ViewHolder(containerView), LayoutContainer{
+                fun bindItem(item:MyFriend) {
+                    txt_FriendName.text=item.nama
+                    txt_FriendEmail.text=item.email
+                    txt_FriendTelp.text=item.telp
+                }
+    }
+}
--- app/src/main/java/com/dina/datateman/MyFriendsFragment.kt
+++ app/src/main/java/com/dina/datateman/MyFriendsFragment.kt
@@ -6,8 +6,51 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_my_friend.*
 
 class MyFriendsFragment : Fragment() {
+    lateinit var listTeman:ArrayList<MyFriend>
+
+    private fun simulasiDataTeman() {
+        listTeman= ArrayList()
+        listTeman.add(
+            MyFriend("Dina Nuraska", "Perempuan", "dina_20510002@Stimata.ac.id",
+        "083845627879", "Palembang")
+        )
+        listTeman.add(
+            MyFriend("Malinda Mahanani", "Perempuan", "malinda_20510008@Stimata.ac.id",
+        "089667916862", "Lamongan")
+        )
+        listTeman.add(
+            MyFriend("Egif Adea", "Perempuan", "egif_20510005@Stimata.ac.id",
+            "081546240013", "Blitar")
+        )
+        listTeman.add(
+            MyFriend("David fathurrahman", "Laki-Laki", "david_20510003@Stimata.ac.id",
+            "085816943202", "Jogja")
+        )
+        listTeman.add(
+            MyFriend("RosyidMuarif", "Laki-Laki", "rosyid_20510009@Stimata.ac.id",
+            "085336334695", "Madiun")
+        )
+        listTeman.add(
+            MyFriend("Isak Irawan", "Laki-Laki", "isak_20310005@Stimata.ac.id",
+                "085735093934", "Blitar")
+        )
+        listTeman.add(
+            MyFriend("Sistahana Sari", "Perempuan", "sistahana_20520015@Stimata.ac.id",
+            "081330336870", "Malang")
+        )
+
+    }
+
+    private fun tampilTeman() {
+        rv_ListMyFriends.layoutManager=LinearLayoutManager(requireActivity())
+        rv_ListMyFriends.adapter=MyFriendAdapter(requireActivity()!!, listTeman)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,5 +65,16 @@ class MyFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView(){
+        simulasiDataTeman()
+        tampilTeman()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
     }
 }
